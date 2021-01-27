219f3f625271a7fb9108a29dbabe42522d4b4778
John Arnold <IohannesArnold@users.noreply.github.com>
2021-01-27 11:43:49 -0600

    Complete stage 2

---
+++ 02-as+ld/default.nix
@@ -0,0 +1,19 @@
+derivation {
+  system = "i686-linux";
+  name = "as+ld";
+  builder = "${import ../03-utils}/bin/sh";
+  # Okay, the above is cheating if we insist that no stage depend on a higher
+  # stage, but it doesn't ruin the bootstrap and makes things more convenient.
+  src = builtins.fetchGit {
+    url = "https://github.com/IohannesArnold/hex2asm";
+    ref = "master";
+    rev = "3849668f74eb68f65cee070960678eebea78b398";
+  };
+  as = import ./as-v2_5.nix;
+  ld = import ./ld-v2_5.nix;
+  args = [ ./build_default.sh ];
+  preferLocalBuild = true;
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
+  outputHash = "0fyhjv5dpfnrg6kqpqgzp0f4kj8gdd053swivqmcn55qsmb5m9cm";
+}
