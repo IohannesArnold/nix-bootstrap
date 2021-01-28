f621b4267751d4744f5c3ded275f1e589c6052d1
John Arnold <IohannesArnold@users.noreply.github.com>
2021-01-27 21:12:00 -0600

    Add stage 4

---
+++ 04-b_compiler/default.nix
@@ -0,0 +1,17 @@
+derivation {
+  system = "i686-linux";
+  name = "B-lang";
+  builder = "${import ../03-utils}/bin/sh";
+  src = builtins.fetchGit {
+    url = "https://github.com/IohannesArnold/asm2c";
+    ref = "master";
+    rev = "c20c719595a6634f3fbe2889f4c43b822f948ec6";
+  };
+  binutils = import ../02-as+ld;
+  utils = import ../03-utils;
+  args = [ ./build.sh ];
+  preferLocalBuild = true;
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
+  outputHash = "1vqr4z0fxwhhslp91gpxmkms7m3g8r1grs9zaiwc8frnk464nima";
+}
